7b2f58acaec7f2a04370b69d3654b68a86a28cdd
Maxim <45520064+Onimuhovich@users.noreply.github.com>
2019-09-12 17:04:52 +0400

    Add files via upload

---
+++ build/precache-manifest.bc9be8d194a4d536435d1b1caacdcf10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d375470bc43006ed36c365d2f6d637d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e56230ec235021906c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "33bad4c9937fb781e7ef",
+    "url": "/static/js/2.4697acaf.chunk.js"
+  },
+  {
+    "revision": "45e56230ec235021906c",
+    "url": "/static/js/main.2798d82d.chunk.js"
+  },
+  {
+    "revision": "35db1dbf86e39a7870e2",
+    "url": "/static/js/runtime~main.1c3b80ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
